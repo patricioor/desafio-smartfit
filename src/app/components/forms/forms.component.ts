a5180d976e3b4c3b8900abde011a0e4f911e663a
Tico <patricio.osterno1@gmail.com>
2024-05-30 15:51:52 -0300

    feat: Create form estructure

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+  results = [];
+
+  constructor() {
+  }
+}
