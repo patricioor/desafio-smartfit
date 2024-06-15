db68c704cae4dcf3321d4350fcf9c1e8c18c124e
Tico <patricio.osterno1@gmail.com>
2024-06-15 18:20:51 -0300

    fix: header width and card list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import {BehaviorSubject} from "rxjs";
 import {CardsListComponent} from "./components/cards-list/cards-list.component";
 import {Location} from "./types/location.interface";
 import {GetUnitsService} from "./services/get-units.service";
+import {SubtitleComponent} from "./components/subtitle/subtitle.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, HttpClientModule, AsyncPipe, JsonPipe, CardsListComponent, NgIf],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent, HttpClientModule, AsyncPipe, JsonPipe, CardsListComponent, NgIf, SubtitleComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
