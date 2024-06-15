08c54b3602d9526df2771ef565ce281ab73b6ee3
Tico <patricio.osterno1@gmail.com>
2024-06-15 16:49:24 -0300

    feat: add subtitle

---
+++ src/app/components/subtitle/subtitle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitle',
+  standalone: true,
+  imports: [],
+  templateUrl: './subtitle.component.html',
+  styleUrl: './subtitle.component.scss'
+})
+export class SubtitleComponent {
+
+}
